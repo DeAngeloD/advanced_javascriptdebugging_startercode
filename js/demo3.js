dda00a30d713a675d83ff79f208d0365acd0b439
DeAngelo <lildeckler@gmail.com>
2017-11-09 00:21:55 +0000

    The console now divides correctly, fixed.

--- js/demo3.js
+++ js/demo3.js
@@ -1,9 +1,8 @@
 // print the result of dividing the given parameters
 // does it look like the programmer understands division?
 function divide(numerator, denominator) {
-    console.log("numerator = " + numerator);
-    console.log("denominator = " + denominator);
-    return denominator/numerator;
+    console.log(numerator + "/" + denominator);
+    return numerator/denominator;
 }
 var answer = divide(10, 2);
 console.log("answer = " + answer);
